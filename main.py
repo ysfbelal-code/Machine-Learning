cf16e7eb299b8bda41c72c7aae89b962ef4836c3
Youssef Belal <228213163+ysfbelal-code@users.noreply.github.com>
2026-05-28 14:47:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,17 @@ API in Python. It detects your hand landmarks, and allows you to play around wit
 I had a lot of fun making it, and I hope you enjoy testing it :). 
             
 I'm 14 years old, and have been learning Python for more than a year now. 
-Although this project itself is impressive, I still wish to learn and improve beyond it. Anyways, here's how to create this project:""")
-
+Although this project itself is impressive, I still wish to learn and improve beyond it. Anyways, here's what you need for this project:""")
+with open("Graphics Collision Detection/hand_landmarker.task", "rb") as task_file:
+    st.download_button("Download hand_landmarker.task from Google AI for Developers (NECESSARY FOR THIS PROJECT)", data=task_file, file_name="hand_landmarker.task")
 with open("physics_using_cv2.py") as code:
     st.code(code.read())
 
 st.text("Output:")
-st.video("output.mp4")
+st.video(r"Graphics Collision Detection\output.mp4")
 st.markdown("That's the project! Because of the new Tasks API that I mentioned earlier, " \
 "all other websites for projects involving hand landmark "
 "detection using mediapipe.solutions are all outdated. As far as I know, this is the only project in the web " \
 "that uses the Tasks API for hand landmark detection.")
+
+st.page_link("https://ai.google.dev/edge/mediapipe/solutions/tasks", label="To learn more about Mediapipe Tasks")
